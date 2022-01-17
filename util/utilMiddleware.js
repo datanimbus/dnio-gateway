d1d5f20b5ab2f5843b3d3ac8be3a1150960eab0f
Jugnu Agrawal <jugnu@appveen.com>
2022-01-17 23:13:04 +0530

    DEF1735

--- util/utilMiddleware.js
+++ util/utilMiddleware.js
@@ -51,7 +51,13 @@ e.checkTokenMiddleware = (_req, _res, _next) => {
 	}
 
 	token = token.split("JWT ")[1];
-	const user = JWT.verify(token, envConfig.TOKEN_SECRET);
+	let user;
+	try {
+		user = JWT.verify(token, envConfig.TOKEN_SECRET);
+	} catch (err) {
+		logger.error(`[${_req.header("txnId")}] Invalid JWT`);
+		return _res.status(401).json({ "message": "Unauthorized" });
+	}
 	if (!user) {
 		logger.error(`[${_req.header("txnId")}] Invalid JWT format`);
 		return _res.status(401).json({ "message": "Unauthorized" });
